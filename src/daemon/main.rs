6263a43fe184c136cbfa58d24c90833c0e746ed3
Vishal Patil <vishpat@gmail.com>
2018-04-27 08:32:54 -0400

    Reorganized the project

--- src/daemon/main.rs
+++ src/daemon/main.rs
@@ -5,10 +5,8 @@ extern crate tokio_core;
 extern crate tokio_uds;
 
 use std::collections::HashMap;
-use std::process::Command;
+
 use std::time::Duration;
-use std::path::Path;
-use std::path::PathBuf;
 use std::fs::File;
 use std::time::Instant;
 
@@ -19,12 +17,7 @@ use tokio_core::reactor::Core;
 use tokio_uds::UnixListener;
 use daemonize::Daemonize;
 
-struct PerfData {
-    cpu_total: f32,
-    cpu_cnt: u32,
-    mem_total: f32,
-    mem_cnt: u32,
-}
+mod procstats;
 
 const MAX_PROCESSES: usize = 5;
 
@@ -33,65 +26,6 @@ static SOCK_FILE: &'static str = "/tmp/psmonitor.sock";
 static STDOUT_FILE: &'static str = "/tmp/psmonitor.stdout";
 static STDERR_FILE: &'static str = "/tmp/psmonitor.stderr";
 
-fn sample_ps(psmap: &mut HashMap<String, PerfData>, total_samples: &mut usize) {
-    let output = Command::new("ps")
-        .arg("aux")
-        .output()
-        .expect("Unable to run ps");
-
-    if !output.status.success() {
-        println!("Problem getting the ps output");
-        return;
-    }
-
-    type PsLine<'a> = (&'a str, f32, f32);
-
-    let ps_aux = String::from_utf8(output.stdout).expect("Unable to ps output");
-
-    let mut ps_aux_trimmed = ps_aux
-        .lines()
-        .skip(1)
-        .map(|x| x.split_whitespace().collect::<Vec<&str>>())
-        .map(|v| {
-            (
-                v.get(10).map_or("", |x| x),
-                v.get(2).map_or(0.0, |x| x.parse().unwrap_or(0.0)),
-                v.get(3).map_or(0.0, |x| x.parse().unwrap_or(0.0)),
-            )
-        })
-        .collect::<Vec<PsLine>>();
-
-    ps_aux_trimmed.sort_by(|a, b| (a.1 as u32).cmp(&(b.1 as u32)).reverse());
-    ps_aux_trimmed.sort_by(|a, b| (a.2 as u32).cmp(&(b.2 as u32)).reverse());
-    ps_aux_trimmed.iter().take(MAX_PROCESSES).for_each(|x| {
-        let perf_data = psmap.entry(x.0.to_string()).or_insert(PerfData {
-            cpu_total: x.1,
-            cpu_cnt: 1,
-            mem_total: x.2,
-            mem_cnt: 1,
-        });
-
-        perf_data.cpu_total += x.1;
-        perf_data.cpu_cnt += 1;
-        perf_data.mem_total += x.2;
-        perf_data.mem_cnt += 1;
-    });
-
-    *total_samples += 1;
-}
-
-fn dump_ps_map(psmap: &HashMap<String, PerfData>, total_samples: usize) {
-    psmap.iter().for_each(|(k, v)| {
-        println!(
-            "{} {} {} {} {}",
-            k,
-            v.cpu_total / v.cpu_cnt as f32,
-            v.cpu_total / total_samples as f32,
-            v.mem_total / v.mem_cnt as f32,
-            v.mem_total / total_samples as f32
-        )
-    });
-}
 
 fn main() {
     let stdout = File::create(STDOUT_FILE).expect("Unable to created stdout file for the daemon");
@@ -105,12 +39,12 @@ fn main() {
 
     let mut core = Core::new().expect("Unable to create tokio core");
 
-    let mut psmap: HashMap<String, PerfData> = HashMap::new();
+    let mut psmap: HashMap<String, procstats::PerfData> = HashMap::new();
     let mut total_samples: usize = 0;
 
     let timer_task = Interval::new(Instant::now(), Duration::from_millis(1000))
-        .for_each(|instant| {
-            sample_ps(&mut psmap, &mut total_samples);
+        .for_each(|_instant| {
+            procstats::sample_ps(&mut psmap, MAX_PROCESSES, &mut total_samples);
             Ok(())
         })
         .map_err(|e| panic!("interval errored; err={:?}", e));
@@ -124,7 +58,7 @@ fn main() {
         .incoming()
         .for_each(|(socket, _)| {
             let buf = Vec::new();
-            let reader = tokio::io::read_to_end(socket, buf)
+            let reader = read_to_end(socket, buf)
                 .map(|(_, _buf)| {
                     println!("incoming: {:?}", String::from_utf8(_buf).unwrap());
                 })
---
+++ src/daemon/procstats.rs
@@ -0,0 +1,56 @@
+use std::process::Command;
+use std::collections::HashMap;
+
+pub struct PerfData {
+    cpu_total: f32,
+    cpu_cnt: u32,
+    mem_total: f32,
+    mem_cnt: u32,
+}
+
+pub fn sample_ps(psmap: &mut HashMap<String, PerfData>, max_processes:usize, total_samples: &mut usize) {
+    let output = Command::new("ps")
+        .arg("aux")
+        .output()
+        .expect("Unable to run ps");
+
+    if !output.status.success() {
+        println!("Problem getting the ps output");
+        return;
+    }
+
+    type PsLine<'a> = (&'a str, f32, f32);
+
+    let ps_aux = String::from_utf8(output.stdout).expect("Unable to ps output");
+
+    let mut ps_aux_trimmed = ps_aux
+        .lines()
+        .skip(1)
+        .map(|x| x.split_whitespace().collect::<Vec<&str>>())
+        .map(|v| {
+            (
+                v.get(10).map_or("", |x| x),
+                v.get(2).map_or(0.0, |x| x.parse().unwrap_or(0.0)),
+                v.get(3).map_or(0.0, |x| x.parse().unwrap_or(0.0)),
+            )
+        })
+        .collect::<Vec<PsLine>>();
+
+    ps_aux_trimmed.sort_by(|a, b| (a.1 as u32).cmp(&(b.1 as u32)).reverse());
+    ps_aux_trimmed.sort_by(|a, b| (a.2 as u32).cmp(&(b.2 as u32)).reverse());
+    ps_aux_trimmed.iter().take(max_processes).for_each(|x| {
+        let perf_data = psmap.entry(x.0.to_string()).or_insert(PerfData {
+            cpu_total: x.1,
+            cpu_cnt: 1,
+            mem_total: x.2,
+            mem_cnt: 1,
+        });
+
+        perf_data.cpu_total += x.1;
+        perf_data.cpu_cnt += 1;
+        perf_data.mem_total += x.2;
+        perf_data.mem_cnt += 1;
+    });
+
+    *total_samples += 1;
+}
